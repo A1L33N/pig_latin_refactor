cb8c60d05acd867701f52cc82612d1a66b163cf1
Guest User <student@epicodus.com>
2015-09-09 13:55:30 -0700

    translates consonants

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ var translateVowels = function(string) {
   var translateArray = [];
   for (var i = 0; i < newArray.length; i++) {
     var vowels = ['a', 'e', 'i', 'o', 'u'];
-    console.log('hi');
+
       if ((vowels.indexOf(newArray[i].charAt(0))) !== -1) {
         translateArray.push(newArray[i] + "ay");
       } else {
@@ -14,4 +14,21 @@ var translateVowels = function(string) {
       }
   };
   return translateArray.join(" ");
+};
+
+var translateConsonants = function(string) {
+  var newArray = string.split(" ");
+  var translateArray = [];
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  for (var i = 0; i < newArray.length; i++) {
+    if (vowels.indexOf(newArray[i].charAt(0)) === -1) {
+      newArray[i] = newArray[i].slice(1) + newArray[i].slice(0, 1);
+      translateArray.push(newArray[i] + "ay");
+
+    } else {
+      translateArray.push(newArray[i]);
+    }
+debugger
+  }
+  return translateArray;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,11 @@
-describe('pigLatin', function() {
+describe('translatesVowels', function() {
   it('translates words that start with vowels', function() {
     expect(translateVowels('aardvarks enter offices')).to.equal('aardvarksay enteray officesay');
+  });
+});
+
+describe('translatesConsonants', function() {
+  it('translates words that start with consonants', function() {
+    expect(translateConsonants('Tigers hunt elephants')).to.equal('igerstay unthay elephants');
   });
 });
