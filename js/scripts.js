259b7efa49f30cf3071fb989f7e66f8cbc935831
Aileen Cacayorin <acacayorin@gmail.com>
2015-09-09 23:33:10 -0700

    refactor using regex for vowels

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,7 @@ var translateVowels = function(string) {
   var newArray = string.split(" ");
   var translateArray = [];
   for (var i = 0; i < newArray.length; i++) {
-    var vowels = ['a', 'e', 'i', 'o', 'u'];
-
-      if ((vowels.indexOf(newArray[i].charAt(0))) !== -1) {
+      if (newArray[i].charAt(0).match(/[aeiouy]/)) {
         translateArray.push(newArray[i] + "ay");
       } else {
         translateArray.push(newArray[i]);
@@ -18,9 +16,8 @@ var translateVowels = function(string) {
 var translateConsonants = function(string) {
   var newArray = (string.toLowerCase().split(" "));
   var translateArray = [];
-  var vowels = ['a', 'e', 'i', 'o', 'u'];
   for (var i = 0; i < newArray.length; i++) {
-    if (vowels.indexOf(newArray[i].charAt(0)) === -1) {
+    if ((newArray[i].charAt(0).match(/[aeiouy]/)) === null) {
       newArray[i] = newArray[i].slice(1) + newArray[i].slice(0, 1);
       translateArray.push(newArray[i] + "ay");
 
